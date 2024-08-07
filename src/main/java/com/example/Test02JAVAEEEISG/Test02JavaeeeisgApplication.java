a6c9aebfe5f658ef4fae6f2d95f62d591ada0170
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-08-07 10:25:19 -0600

    First commit

---
+++ src/main/java/com/example/Test02JAVAEEEISG/Test02JavaeeeisgApplication.java
@@ -0,0 +1,13 @@
+package com.example.Test02JAVAEEEISG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test02JavaeeeisgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test02JavaeeeisgApplication.class, args);
+	}
+
+}
